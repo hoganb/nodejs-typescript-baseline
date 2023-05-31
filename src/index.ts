85efcbfe26f2635e95324fe46e644a5d0988a16e
Ben Hogan <sweeetata@gmail.com>
2023-05-31 15:08:41 +0100

    Recommended extensions + config

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+/**
+ * Says hello world
+ * * Information
+ * ! Warning
+ * ? Question
+ * TODO: xyz
+ * @returns string
+ */
 export const sayHelloWorld = (): string => {
   return `Hello World`;
 };
